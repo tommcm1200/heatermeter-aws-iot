dcdc5e17d2d5533c3edb6b3a7abb4338756495a0
Tom McMeekin <tommcm@amazon.com>
2018-11-02 11:01:22 +1100

    updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # heatermeter-aws-iot
 
+git submodule add -b master https://github.com/aws/aws-iot-device-sdk-python.git submodules/aws-iot-device-sdk-python
+
 ## Required Libraries
 sseclient 0.0.19
 https://pypi.org/project/sseclient/
---
+++ create-aws-iot-stack.sh
@@ -0,0 +1,8 @@
+aws cloudformation create-stack \
+  --stack-name heatermeter-`echo $(date +%s)` \
+  --template-body file://./templates/iot-gateway.yaml \
+  --disable-rollback \
+    --parameters \
+    "[{ \"ParameterKey\": \"HeatermeterThingID\", \"ParameterValue\": \"B40429203C26\" }, 
+      { \"ParameterKey\": \"CertificateARN\", \"ParameterValue\": \"arn:aws:iot:ap-southeast-2:447119549480:cert/f9f6aa49c3a43098161bfda845084adc32f3c0013e81d162c3d04f8881bdcd0e\" } ]" \
+        --capabilities CAPABILITY_NAMED_IAM
